5c15c537ac3fd8a1b4d1b6807c261e8da585849c
vasu <vasu.cheemakurthi@cdk.com>
2022-08-03 18:21:54 +0530

    STAGE..

--- dbscripts/Salesforce/CopyCommands/0001_Copy.sql
+++ dbscripts/Salesforce/CopyCommands/0001_Copy.sql
@@ -3,7 +3,7 @@
 --comment: example comment
 copy into dev_db.test_schema.SF_ACCOUNT
 from 
-@prod.workday.aws_stage2
+@sampledb.test_schema.aws_stage2
 file_format = (type = csv field_delimiter = '|' skip_header = 0)
 pattern='.*Account2.*'
 force = TRUE;
