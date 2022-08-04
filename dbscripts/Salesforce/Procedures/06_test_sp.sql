ad4ea3c749b53fb8e37523217155cc5aa40cd8d9
vasu <vasu.cheemakurthi@cdk.com>
2022-08-04 12:33:01 +0530

    coiit staged changes

---
+++ dbscripts/Salesforce/Procedures/06_test_sp.sql
@@ -0,0 +1,19 @@
+--changeset vasu:291 labels:New SP context:example-context endDelimiter:"\$\$"
+--comment: example comment
+--USE SCHEMA TEST_SCHEMA;
+create or replace procedure TEST_SCHEMA.myprocedure8()
+  returns varchar
+  language sql
+  as
+ $$
+    -- Snowflake Scripting code
+    declare
+      radius_of_circle float;
+      area_of_circle float;
+    begin
+      radius_of_circle := 3;
+      area_of_circle := pi() * radius_of_circle * radius_of_circle;
+      return area_of_circle;
+    end;
+  $$
+  ;
